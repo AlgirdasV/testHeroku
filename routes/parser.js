3aa32db6215e603838c99fdf064b552c50e38260
LukasKairys <lukas.kairys@hotmail.com>
2014-03-10 15:20:51 +0200

    Small changes in Parser

--- routes/parser.js
+++ routes/parser.js
@@ -1,25 +1,16 @@
-// var validator = require('./validator');
+//var validator = require('./validator');
 
 var Parser = function() {
 
 
     this.parseObject = function(JSONstringified) {
         var obj = this.jsonParser(JSONstringified);
-        var userID = this.getUserID(obj);
-        var actions = this.getActions(obj);
-        validateUserID();
+        validateOjbect(obj);
+        dataRecorder(obj);
     };
 
     this.jsonParser = function(JSONstringified) {
         return JSON.parse(JSONstringified);
-    };
-
-    this.getUserID = function(json) {
-        return json.userID;
-    };
-
-    this.getActions = function(JSONobject) {
-        return JSONobject.actions;
     };
 
 };
