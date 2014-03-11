c74868d91a410e7eaa792c89aec27ffd6549d5dd
LukasKairys <lukas.kairys@hotmail.com>
2014-03-11 15:16:48 +0200

    Validator finished, extra info in description
    Sends message then validation fails, validation succeeds or validated
    with errors.

--- routes/validator.js
+++ routes/validator.js
@@ -2,17 +2,38 @@ var inspector = require('schema-inspector');
 
 var Validator = function() {
 
+    var objectSchema = {
+        type: 'object',
+        strict: true,
+        properties: {
+            userID: {
+                type: 'string',
+                exec: function(schema, post) {
+                    var splits = post.split(':'),
+                        userIP = splits[0],
+                        ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+                    if (!userIP.match(ipformat)) {
+                        this.report('IP is not valid ');
+                    }
+                }
+            },
+            actions: {
+                type: 'array'
+            }
+        }
+    };
+
   var clickSchema = {
       type: 'object',
       strict: true,
     properties: {
           eventType: { type:'string'},
-          positionX: { type:'number'},
-          positionY: { type:'number'},
+          positionX: { type:'number', gte: 0},
+          positionY: { type:'number', gte: 0},
           documentHeight: { type: 'null'},
           documentWidth: {type: 'null'},
           elementID: { type: 'string'},
-          timeNow:   { type: 'number'}
+          timeNow:   { type: 'number', lt: Date.now(), gt: 0}
          }
   };
 
@@ -21,12 +42,12 @@ var Validator = function() {
       strict: true,
     properties: {
           eventType: { type: 'string'},
-          positionX: { type: 'number'},
-          positionY: { type: 'number'},
+          positionX: { type:'number', gte: 0},
+          positionY: { type:'number', gte: 0},
           documentHeight: { type: 'null'},
           documentWidth: {type: 'null'},
           elementID: { type: 'string'},
-          timeNow:   { type: 'number'}
+          timeNow:   { type: 'number', lt: Date.now(), gt: 0}
          }
   };
 
@@ -37,10 +58,10 @@ var Validator = function() {
           eventType: { type:'string'},
           positionX: { type:'null'},
           positionY: { type:'null'},
-          documentHeight: { type: 'number'},
-          documentWidth: {type: 'number'},
+          documentHeight: { type: 'number', gt: 0},
+          documentWidth: {type: 'number', gt: 0},
           elementID: {type: null},
-          timeNow:   { type: 'number'}
+          timeNow:   { type: 'number', lt: Date.now(), gt: 0}
          }
   };
 
@@ -49,64 +70,77 @@ var Validator = function() {
         strict: true,
     properties: {
           eventType: { type:'string'},
-          positionX: { type:'number'},
-          positionY: { type:'number'},
+          positionX: { type:'number', gte: 0},
+          positionY: { type:'number', gte: 0},
           documentHeight: { type: 'null'},
           documentWidth: {type: 'null'},
           elementID: { type: 'null'},
-          timeNow:   { type: 'number'}
+          timeNow:   { type: 'number', lt: Date.now(), gt: 0}
          }
   };
 
-this.validateUserID = function(object) {
-    var splits = object.userID.split(':'),
-        userIP = splits[0],
-        ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    if(userIP.match(ipformat)) {
-        console.log('TRUE');
-    } else  {
-        console.log('IP is not valid ');
+  this.validate = function(object) {
+    var objectResult = inspector.validate(objectSchema, object);
+    var actionsWithErrors = [];
+    if (!objectResult.valid) {
+        //publish('validation failed');
+        console.log('Validation failed');
+    } else {
+        var actions = object.actions;
+        for (var i = actions.length - 1; i >= 0; i--) {
+          var result = undefined;
+          var validated = false;
+          if (actions[i].eventType ==='click' || actions[i].eventType ==='dblclick' || actions[i].eventType ==='dragstart' || actions[i].eventType ==='drop'){
+            result = inspector.validate(clickSchema,actions[i]); // Candidate is not valid
+            validated = true;
+          }
+          if (actions[i].eventType ==='focus'){
+            result = inspector.validate(focusSchema,actions[i]);
+            validated = true;
+          }
+          if (actions[i].eventType ==='resize' || actions[i].eventType ==='startScreen'){
+            result = inspector.validate(screenSchema,actions[i]);
+            validated = true;
+          }
+          if (actions[i].eventType ==='scroll'){
+            result = inspector.validate(scrollSchema,actions[i]);
+            validated = true;
+          }
+          //console.log(result.format());
+          if(!validated || !result.valid){
+              actionsWithErrors.push(i);
+          }
+
+        }
+        if (actionsWithErrors.length === 0) {
+            //publish('validation succeeded');
+            console.log('validation succeeded');
+        }
+        else {
+            //publish('validated with errors', actionsWithErrors);
+            console.log('validated with errors');
     }
 
-};
-
-  this.validate = function(object) {
-    var actions = object.actions;
-    for (var i = actions.length - 1; i >= 0; i--) {
-      var result = undefined;
-      if (actions[i].eventType ==='click' || actions[i].eventType ==='dblclick' || actions[i].eventType ==='dragstart' || actions[i].eventType ==='drop'){
-        result = inspector.validate(clickSchema,actions[i]); // Candidate is not valid
-      }
-      if (actions[i].eventType ==='focus'){
-        result = inspector.validate(focusSchema,actions[i]);
-      }
-      if (actions[i].eventType ==='resize' || actions[i].eventType ==='startScreen'){
-        result = inspector.validate(screenSchema,actions[i]);
-      }
-      if (actions[i].eventType ==='scroll'){
-        result = inspector.validate(scrollSchema,actions[i]);
-      }
-      if (result !== undefined)
-        console.log(result.format());
-    };
-  }
-
+    }
+    
+  };
 };
 
 
 // object for testing:
 var object = {
-  userID: '33.3333.33.3:Chrome',
-  actions: [ {positionX: 12, positionY:145, eventType: 'click', elementID: 'form1', timeNow: Date.now()},
-   {positionX: 111, eventType: 'focus', positionY:222, elementID: 'form1', timeNow: Date.now()},
-   {positionX: 0, eventType: 'scroll', positionY: 145, elementID: null, timeNow: Date.now()},
-   {positionX: null, eventType: 'resize', positionY: null, elementID: null, documentHeight: 1453,documentWidth: 364, timeNow: Date.now()},
-   {positionX: null, eventType: 'resize', positionY: null, elementID: null, timeNow: Date.now()}
+  userID: '33.23.33.3:Chrome',
+  // userID: '',
+  actions: [ {positionX: 12, positionY:145, eventType: 'click', elementID: 'form1', documentHeight: null,documentWidth: null,timeNow: Date.now()-1},
+   {positionX: 111, eventType: 'focus', positionY:222, elementID: 'form1', documentHeight: null,documentWidth: null, timeNow: Date.now()-1},
+   {positionX: 0, eventType: 'scroll', positionY: 145, elementID: null, documentHeight: null,documentWidth: null, timeNow: Date.now()-1},
+   {positionX: null, eventType: 'resize', positionY: null, elementID: null, documentHeight: 1453,documentWidth: 364, timeNow: Date.now()-1},
+   {positionX: null, eventType: 'resize', positionY: null, elementID: null, documentHeight: 1453,documentWidth: 364, timeNow: Date.now()-1}
    ]
 };
 
 validator = new Validator();
 
-validator.validateUserID(object);
+// validator.validateUserID(object);
 validator.validate(object);
 module.exports = Validator;
