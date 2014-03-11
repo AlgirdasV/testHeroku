c5cd197cad426a4bb3f8619190a18e79bd525758
povsim <povilozauras@gmail.com>
2014-03-11 16:32:16 +0200

    Added Cors
    cors is now enabled

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 /**
  *  Module dependencies.
  */
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 
 var express = require('express');
 var routes = require('./routes');
@@ -18,6 +31,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(allowCrossDomain);
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
